b619ec5c3931b6497062766c9ba8e123683a4ecb
tpiros <2042718+tpiros@users.noreply.github.com>
2020-03-18 15:51:45 +0100

    Initial commit from gatsby: (https://github.com/tpiros/unstack-gatsby-starter.git)

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <main className="">{children}</main>
+    </>
+  )
+}
+
+export default Layout
